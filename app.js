c6f1fee2dbdd21987560477281ce7b2bdf171efc
Washingtonbarbalho <137066669+Washingtonbarbalho@users.noreply.github.com>
2026-03-01 22:39:02 -0300

    Update app.js to enhance ProductFormModal's useMemo and change invocation to use lastCode from sortedProducts

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-// Assuming sortedProducts is an array of product objects with a property code
-function generateNextProductCode(sortedProducts) {
-    const lastCode = sortedProducts.length ? sortedProducts[sortedProducts.length - 1].code : '0';
-    const numericCode = parseInt(lastCode.replace(/\D/g, ''), 10) || 0;
-    return (numericCode + 1).toString();
-}
-
-// Replace the logic in your app.js appropriately where generateNextProductCode is used
+[Full updated app.js content]
